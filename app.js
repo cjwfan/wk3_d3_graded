809216d2599cd77dd80a2c35b53663bbba61f751
Mia C <mcollette101@gmail.com>
2026-02-20 16:38:10 -0600

    added html and Dom Selection

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const output = document.getElementById("output");
+const output2 = document.getElementById("output2");
+const output3 = document.getElementById("output3");
+
+const jsonForm = document.querySelector("#jsonForm");
+
